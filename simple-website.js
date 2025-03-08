91dabdbe977439bfc18f0ce273aec54b60c370a9
Ayman Qaisari <qaisariayman@gmail.com>
2025-03-08 23:41:32 +0100

    First commit
    Simple website project for learning html and css

---
+++ simple-website.js
@@ -0,0 +1,10 @@
+// simple-website.js
+
+// Function to display a greeting message
+function displayGreeting() {
+    const greeting = "Hello, welcome to my simple website!";
+    console.log(greeting);
+}
+
+// Call the function to display the greeting
+displayGreeting();
